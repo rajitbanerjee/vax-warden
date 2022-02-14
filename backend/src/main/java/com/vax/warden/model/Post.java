39e26a53dca2fa5c86b19ab2b064bac1ef9d7353
ConorHK <conorknowles@gmail.com>
2022-02-14 12:49:02 +0000

    Adjust constraints on Post attributes
    Includes auto timestamping and conversion of contest from VARCHAR(255)
    to LONGTEXT

--- backend/src/main/java/com/vax/warden/model/Post.java
+++ backend/src/main/java/com/vax/warden/model/Post.java
@@ -6,22 +6,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 @Entity
 @Table(name = "forum")
 public class Post {
@@ -29,17 +25,15 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    @NotNull
-    @NonNull
-    private Date date;
+    @CreationTimestamp private Date timestamp;
 
     // TODO nit: convert to camelCase https://tknk.io/wuip
     private Long reply_id;
 
-    @NotNull @NonNull private Long user_id;
+    @NotNull private Long user_id;
 
-    @NotBlank @NonNull private String content;
+    @NotBlank
+    @Lob
+    @Column(length = 100000)
+    private String content;
 }
