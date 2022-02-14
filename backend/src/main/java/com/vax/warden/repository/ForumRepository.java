d505b796209168fad033c452d1f9dc35d651a8ea
ConorHK <conorknowles@gmail.com>
2022-02-14 11:39:02 +0000

    Change Forum class to Post

--- backend/src/main/java/com/vax/warden/repository/ForumRepository.java
+++ backend/src/main/java/com/vax/warden/repository/ForumRepository.java
@@ -1,7 +1,6 @@
 package com.vax.warden.repository;
 
-import com.vax.warden.model.Forum;
-import java.util.Optional;
+import com.vax.warden.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ForumRepository extends JpaRepository<Forum, Long> {}
+public interface ForumRepository extends JpaRepository<Post, Long> {}
