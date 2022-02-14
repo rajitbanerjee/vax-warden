6582cb82bf1caeea413dd8e8e8015e3a7ec00f05
ConorHK <conorknowles@gmail.com>
2022-02-14 12:15:53 +0000

    Add http requests for the forum
    Restructured requests to be in respective folders

--- backend/src/main/java/com/vax/warden/model/Post.java
+++ backend/src/main/java/com/vax/warden/model/Post.java
@@ -1,20 +1,27 @@
 package com.vax.warden.model;
 
 import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "forum")
 public class Post {
@@ -22,12 +29,17 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull private Date date;
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    @NotNull
+    @NonNull
+    private Date date;
 
     // TODO nit: convert to camelCase https://tknk.io/wuip
     private Long reply_id;
 
-    @NotBlank private User user;
+    @NotNull @NonNull private Long user_id;
 
-    @NotBlank private String content;
+    @NotBlank @NonNull private String content;
 }
