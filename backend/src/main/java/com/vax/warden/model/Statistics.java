a6fca9e491fecacda7f61f995ebbe92f9055d869
ConorHK <conorknowles@gmail.com>
2022-03-02 18:18:24 +0000

    Adjust naming scheme to match Vaccination variables

--- backend/src/main/java/com/vax/warden/model/Statistics.java
+++ backend/src/main/java/com/vax/warden/model/Statistics.java
@@ -1,54 +1,82 @@
 package com.vax.warden.model;
 
-import java.util.Map;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
+import lombok.Data;
 
+@Data
 public class Statistics {
 
-  private Map<String, Integer> centre;
-  private Map<Date, Integer> firstAppointmentDate;
-  private Map<Date, Integer> secondAppointmentDate;
-  private Map<VaccineType, Integer> firstVaccineType;
-  private Map<VaccineType, Integer> secondVaccineType;
-  private Map<Integer, Integer> dosesRecieved;
+    private Map<String, Integer> centre;
+    private Map<Date, Integer> firstAppointment;
+    private Map<Date, Integer> secondAppointment;
+    private Map<VaccineType, Integer> firstVaccineType;
+    private Map<VaccineType, Integer> secondVaccineType;
+    private Map<Integer, Integer> dosesRecieved;
 
-  Statistics() {
-    this.centre = new HashMap<String, Integer>();
-    this.firstAppointmentDate = new HashMap<Date, Integer>();
-    this.secondAppointmentDate = new HashMap<Date, Integer>();
-    this.firstVaccineType = new HashMap<VaccineType, Integer>();
-    this.secondVaccineType = new HashMap<VaccineType, Integer>();
-    this.dosesRecieved = new HashMap<Integer, Integer>();
-  }
+    public Statistics() {
+        this.centre = new HashMap<String, Integer>();
+        this.firstAppointment = new HashMap<Date, Integer>();
+        this.secondAppointment = new HashMap<Date, Integer>();
+        this.firstVaccineType = new HashMap<VaccineType, Integer>();
+        this.secondVaccineType = new HashMap<VaccineType, Integer>();
+        this.dosesRecieved = new HashMap<Integer, Integer>();
+    }
 
-  public void incrementCentre(String centre) {
-    this.centre.putIfAbsent(centre, 0);
-    this.centre.put(centre, this.centre.get(centre));
-  }
+    private void incrementCentre(String centre) {
+        this.centre.putIfAbsent(centre, 0);
+        this.centre.put(centre, this.centre.get(centre) + 1);
+    }
 
-  public void incrementFirstAppointmentDate(Date firstAppointmentDate) {
-    this.firstAppointmentDate.putIfAbsent(firstAppointmentDate, 0);
-    this.firstAppointmentDate.put(firstAppointmentDate, this.firstAppointmentDate.get(firstAppointmentDate));
-  }
+    private void incrementFirstAppointment(Date firstAppointment) {
+        if (firstAppointment == null) {
+            return;
+        }
+        this.firstAppointment.putIfAbsent(firstAppointment, 0);
+        this.firstAppointment.put(
+                firstAppointment, this.firstAppointment.get(firstAppointment) + 1);
+    }
 
-  public void incrementSecondAppointmentDate(String secondAppointmentDate) {
-    this.secondAppointmentDate.putIfAbsent(secondAppointmentDate, 0);
-    this.secondAppointmentDate.put(secondAppointmentDate, this.secondAppointmentDate.get(secondAppointmentDate));
-  }
+    private void incrementSecondAppointment(Date secondAppointment) {
+        if (secondAppointment == null) {
+            return;
+        }
+        this.secondAppointment.putIfAbsent(secondAppointment, 0);
+        this.secondAppointment.put(
+                secondAppointment, this.secondAppointment.get(secondAppointment) + 1);
+    }
 
-  public void incrementFirstVaccineType(VaccineType firstVaccineType) {
-    this.firstVaccineType.putIfAbsent(firstVaccineType, 0);
-    this.firstVaccineType.put(firstVaccineType, this.firstVaccineType.get(firstVaccineType));
-  }
+    private void incrementFirstVaccineType(VaccineType firstVaccineType) {
+        if (firstVaccineType == null) {
+            return;
+        }
+        this.firstVaccineType.putIfAbsent(firstVaccineType, 0);
+        this.firstVaccineType.put(
+                firstVaccineType, this.firstVaccineType.get(firstVaccineType) + 1);
+    }
 
-  public void incrementSecondVaccineType(VaccineType secondVaccineType) {
-    this.secondVaccineType.putIfAbsent(secondVaccineType, 0);
-    this.secondVaccineType.put(secondVaccineType, this.secondVaccineType.get(secondVaccineType));
-  }
+    private void incrementSecondVaccineType(VaccineType secondVaccineType) {
+        if (secondVaccineType == null) {
+            return;
+        }
+        this.secondVaccineType.putIfAbsent(secondVaccineType, 0);
+        this.secondVaccineType.put(
+                secondVaccineType, this.secondVaccineType.get(secondVaccineType) + 1);
+    }
 
-  public void incrementDosesRecieved(int dosesRecieved) {
-    this.dosesRecieved.putIfAbsent(dosesRecieved, 0);
-    this.dosesRecieved.put(dosesRecieved, this.dosesRecieved.get(dosesRecieved));
-  }
+    private void incrementDosesRecieved(int dosesRecieved) {
+        this.dosesRecieved.putIfAbsent(dosesRecieved, 0);
+        this.dosesRecieved.put(dosesRecieved, this.dosesRecieved.get(dosesRecieved));
+    }
+
+    public Statistics tallyVaccination(Vaccination vaccination) {
+        incrementCentre(vaccination.getCentre());
+        incrementFirstAppointment(vaccination.getFirstAppointment());
+        incrementSecondAppointment(vaccination.getSecondAppointment());
+        incrementFirstVaccineType(vaccination.getFirstVaccineType());
+        incrementSecondVaccineType(vaccination.getSecondVaccineType());
+        incrementDosesRecieved(vaccination.getDosesReceived());
+        return this;
+    }
 }
