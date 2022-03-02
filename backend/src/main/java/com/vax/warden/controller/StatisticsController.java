9e4343bdc4307f2de2b606691de42b7d04eadd9c
ConorHK <conorknowles@gmail.com>
2022-03-02 18:19:32 +0000

    Implement controller

--- backend/src/main/java/com/vax/warden/controller/StatisticsController.java
+++ backend/src/main/java/com/vax/warden/controller/StatisticsController.java
@@ -1,1 +1,38 @@
+package com.vax.warden.controller;
 
+import com.vax.warden.model.Statistics;
+import com.vax.warden.service.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/statistics")
+public class StatisticsController {
+    private final StatisticsService statisticsService;
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public Statistics getStatistics() {
+        Statistics statistics = statisticsService.aggregateAll();
+        System.out.println(statistics);
+        return statisticsService.aggregateAll();
+    }
+
+    @GetMapping("/vaccination/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Statistics getStatisticsById(@PathVariable long id) {
+        return statisticsService.getStatisticsById(id);
+    }
+
+    @GetMapping("/user/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Statistics getStatisticsByUserId(@PathVariable long id) {
+        return statisticsService.getStatisticsByUserId(id);
+    }
+}
