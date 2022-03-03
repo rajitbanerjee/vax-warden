de07e6dfc50743e53ef733c99ac8be1f359a9c8a
ConorHK <conorknowles@gmail.com>
2022-03-03 00:12:49 +0000

    Refactor forum controller to use a service

--- backend/src/main/java/com/vax/warden/controller/ForumController.java
+++ backend/src/main/java/com/vax/warden/controller/ForumController.java
@@ -1,11 +1,10 @@
 package com.vax.warden.controller;
 
-import com.vax.warden.exception.ResourceNotFoundException;
 import com.vax.warden.model.Post;
-import com.vax.warden.repository.ForumRepository;
+import com.vax.warden.service.ForumService;
 import java.util.List;
 import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,28 +15,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/forum")
 public class ForumController {
 
-    @Autowired ForumRepository forumRepository;
+    private final ForumService forumService;
 
     @PostMapping("/post")
     @ResponseStatus(HttpStatus.CREATED)
     public Post post(@Valid @RequestBody Post post) {
-        return forumRepository.save(post);
+        return forumService.save(post);
     }
 
     @GetMapping("/list")
     @ResponseStatus(HttpStatus.OK)
     public List<Post> list() {
-        return forumRepository.findAll();
+        return forumService.findAll();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Post getPostById(@PathVariable long id) {
-        return forumRepository
-                .findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("No post found with id = " + id));
+    public Post getPostById(@PathVariable Long id) {
+        return forumService.getPostById(id);
     }
 }
