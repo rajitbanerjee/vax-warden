265c4dba33f397f283deace29d249bc1f075aa18
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-03-09 02:03:35 +0530

    Minor refactoring and list styling

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,22 +1,24 @@
-import { Heading, Text, VStack, UnorderedList, ListItem } from "@chakra-ui/react";
+import { Heading, List, ListIcon, ListItem, Text, VStack } from "@chakra-ui/react";
 import * as statistics from "client/statistics";
 import { Statistics } from "client/types";
 import { formatDate } from "client/util";
 import useAuth from "hooks/useAuth";
 import { useEffect, useState } from "react";
-import type { Response } from "redaxios";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { MdCheckCircle } from "react-icons/md";
 
 const doseString = (stats: Statistics | undefined): string => {
   let doses = 0;
-  if (stats !== undefined) {
-    if (stats.dosesReceived[2] !== undefined) {
+  if (stats) {
+    if (stats.dosesReceived[2]) {
       doses = 2;
-    } else if (stats.dosesReceived[1] !== undefined) {
+    } else if (stats.dosesReceived[1]) {
       doses = 1;
     }
   }
-  return `DOSES RECEIVED: ${doses}`;
+  return `Doses Received: ${doses}`;
 };
+
 const formatKey: { [k: string]: any } = {
   centre: "Registered for vaccination at",
   firstAppointment: "First appointment scheduled for",
@@ -26,47 +28,36 @@ const formatKey: { [k: string]: any } = {
 };
 
 const noBookingIndicator = "TBD";
-const formatValue = (k: string, v: any): string => {
-  if (k.includes("Appointment") && v !== noBookingIndicator) return formatDate(v).split(":00")[0];
-  return v;
-};
 
 const formatEntry = (k: string, v: any): string => {
+  const formatValue = (k: string, v: any): string => {
+    if (k.includes("Appointment") && v !== noBookingIndicator) return formatDate(v).split(":00")[0];
+    return v;
+  };
+
   let value = Object.keys(v)[0];
-  if (typeof value !== "string") {
-    value = noBookingIndicator;
-  }
-  return `${formatKey[k]} ${formatValue(k, value)}`;
+  if (typeof value !== "string") value = noBookingIndicator;
+  return `${formatKey[k]} ${formatValue(k, value)}.`;
 };
 
 const entries = (stats: Statistics | undefined): [string, any][] => {
-  if (stats === undefined) {
-    return [];
-  }
+  if (!stats) return [];
   return Object.entries(stats);
 };
+
+const keysToHide = ["dosesReceived", "nationality", "gender"];
+
 export const Home: React.FC = (): JSX.Element => {
   const { currentUser, jwtToken } = useAuth();
   const [stats, setStats] = useState<Statistics | undefined>(undefined);
 
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<Response<any> | null>();
-
   const fetchData = (token: string) => {
-    setLoading(true);
-    statistics
-      .getForUser(token)
-      .then((newStats) => setStats(newStats))
-      .catch((e) => setError(e))
-      .finally(() => setLoading(false));
+    statistics.getForUser(token).then((newStats) => setStats(newStats));
   };
 
   useEffect(() => {
-    window.scrollTo(0, 0);
     fetchData(jwtToken);
   }, [jwtToken]);
-
-  const keysToHide = ["dosesReceived", "nationality", "gender"];
 
   return (
     <VStack spacing={5} pb="200px">
@@ -75,13 +66,20 @@ export const Home: React.FC = (): JSX.Element => {
       </Heading>
       <Text>Date of Birth: {formatDate(currentUser.dateOfBirth)}</Text>
       <Text style={{ fontWeight: "bold" }}>{doseString(stats)}</Text>
-      <UnorderedList>
+      <List>
         {entries(stats)
           .filter(([k, _]) => !keysToHide.includes(k))
-          .map(([k, v]) => (
-            <ListItem>{formatEntry(k, v)}</ListItem>
-          ))}
-      </UnorderedList>
+          .map(([k, v]) => {
+            const entry = formatEntry(k, v);
+            const isDone = !entry.includes(noBookingIndicator);
+            return (
+              <ListItem>
+                <ListIcon as={isDone ? MdCheckCircle : AiOutlineClockCircle} color={isDone ? "green" : "grey"} />
+                {entry}
+              </ListItem>
+            );
+          })}
+      </List>
     </VStack>
   );
 };
