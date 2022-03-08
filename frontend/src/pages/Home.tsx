496b7c6998991228d865b79f9fb36e77a46374c4
Conor Knowles <ck@conorknowles.com>
2022-03-08 16:01:52 +0000

    WIP Home history display

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,33 @@
-import { Text } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { Heading, VStack, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useAuth from "hooks/useAuth";
+import * as vaccination from "client/vaccination";
+import { Vaccination } from "client/types";
 
 export const Home: React.FC = (): JSX.Element => {
+  const { currentUser, jwtToken } = useAuth();
+  const [count, setCount] = useState(0);
+  const [vax, setVax] = useState<Vaccination | undefined>(undefined);
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+    const fetchData = async (id: number, jwtToken: string) => {
+      const response = await vaccination.get(id, jwtToken);
+      setVax(response);
+    };
+    fetchData(currentUser.id!, jwtToken).catch(console.error);
+    setCount(1);
+  }, [currentUser.id, jwtToken, count]);
 
-  return <Text>Hello, Home!</Text>;
+  return (
+    <VStack spacing={5} pb="200px">
+      <Heading size="md" textAlign="center">
+        {currentUser.firstName} {currentUser.lastName}
+      </Heading>
+      <Text>{new Date(currentUser.dateOfBirth).toUTCString().split("00:")[0]}</Text>{" "}
+      {/** TODO remove, used for getting token to make vaccine booking**/}
+      <Text>{currentUser.jwtToken}</Text>
+      <Text>{vax!.centre}</Text>
+    </VStack>
+  );
 };
