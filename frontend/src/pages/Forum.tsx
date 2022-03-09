ff643ce02331531ef26ea41f7f54fc676a7975e9
ConorHK <dev@conorknowles.com>
2022-03-09 20:26:37 +0000

    Initial display of forum data on page

--- frontend/src/pages/Forum.tsx
+++ frontend/src/pages/Forum.tsx
@@ -1,5 +1,25 @@
-import { Text } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
+import * as forum from "client/forum";
+import { formatDate } from "client/util";
+import { Post } from "client/types";
+import { useEffect, useState } from "react";
+import useAuth from "hooks/useAuth";
 
 export const Forum: React.FC = (): JSX.Element => {
-  return <Text>Hello, Forum!</Text>;
+  const { currentUser, jwtToken } = useAuth();
+  const [posts, setPosts] = useState<Post | undefined>(undefined);
+
+  const fetchData = (token: string) => {
+    forum.getPosts(token).then((newPosts) => setPosts(newPosts));
+  };
+
+  useEffect(() => {
+    fetchData(jwtToken);
+  }, [jwtToken]);
+
+  return (
+    <VStack spacing={5} pb="200px">
+      <Text>{JSON.stringify(posts)}</Text>
+    </VStack>
+  );
 };
