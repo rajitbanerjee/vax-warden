102f526b922ecc0c391e94eac434dc5a6a5d6f4a
ConorHK <dev@conorknowles.com>
2022-03-09 14:46:44 +0000

    Vaccination bookings by Centre graph

--- frontend/src/pages/Statistics.tsx
+++ frontend/src/pages/Statistics.tsx
@@ -1,8 +1,17 @@
-import { Text } from "@chakra-ui/react";
+import { Text, VStack, Heading } from "@chakra-ui/react";
 import * as statistics from "client/statistics";
 import { useEffect, useState } from "react";
 import { Stats } from "client/types";
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries } from "react-vis";
+import { XYPlot, VerticalBarSeries, XAxis, YAxis, LabelSeries, VerticalBarSeriesPoint } from "react-vis";
+
+const centre = (stats: Stats | undefined): (any[] | VerticalBarSeriesPoint)[] => {
+  if (!stats) return [];
+  let points: any[] | VerticalBarSeriesPoint = [];
+  Object.entries(stats.centre).forEach(([key, value]) => {
+    points.push({ x: key, y: value });
+  });
+  return points;
+};
 
 export const Statistics: React.FC = (): JSX.Element => {
   const [stats, setStats] = useState<Stats | undefined>(undefined);
@@ -15,5 +24,21 @@ export const Statistics: React.FC = (): JSX.Element => {
     fetchData();
   }, []);
 
-  return <Text>{JSON.stringify(stats)}</Text>;
+  const chartWidth = 900;
+  const chartHeight = 200;
+  const chartDomain = [0, 10];
+
+  return (
+    <VStack spacing={5} pb="200px">
+      <Text>{JSON.stringify(stats)}</Text>
+      <Heading size="md" textAlign="center">
+        Vaccination bookings by Centre
+      </Heading>
+      <XYPlot width={chartWidth} height={chartHeight} yDomain={chartDomain} xType="ordinal">
+        <XAxis />
+        <YAxis />
+        <VerticalBarSeries data={centre(stats)} barWidth={0.8} />
+      </XYPlot>
+    </VStack>
+  );
 };
