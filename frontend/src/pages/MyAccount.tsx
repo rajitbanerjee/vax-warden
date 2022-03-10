e12cdac7bd88c3eabe2bc7d5436a41d2d63040c5
ConorHK <dev@conorknowles.com>
2022-03-10 02:39:52 +0000

    Include age group key

--- frontend/src/pages/MyAccount.tsx
+++ frontend/src/pages/MyAccount.tsx
@@ -14,6 +14,7 @@ export const formatUserDetailsKey: UserDetailsKeys = {
   nationality: "Nationality",
   gender: "Gender",
   password: "Password", // Not used in MyAccount, but instead in Login
+  ageGroup: "Age Group",
 };
 
 const formatUserDetailsValue = (k: string, v: any): string => {
