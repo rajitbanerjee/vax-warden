63d47fce8e8f5d9a137bc1ac9385cbbaa26e642c
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-03-12 04:16:25 +0530

    Minor fix: mvn install formatting

--- backend/src/main/java/com/vax/warden/config/FlywayConfiguration.java
+++ backend/src/main/java/com/vax/warden/config/FlywayConfiguration.java
@@ -1,10 +1,9 @@
 package com.vax.warden.config;
 
+import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class FlywayConfiguration {
