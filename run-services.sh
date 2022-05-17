f9ee8f8c8434a93410e410b7aa5ba8a2687fcf7b
AmplifiedHuman <amplifiedhuman@gmail.com>
2022-05-18 01:32:26 +0800

    Add https support

--- run-services.sh
+++ run-services.sh
@@ -2,6 +2,13 @@
 
 num_args=$#
 
+mkcert -install
+mkdir ./frontend/.cert
+# generate frontend certificate
+mkcert -key-file ./frontend/.cert/key.pem -cert-file ./frontend/.cert/cert.pem "localhost"
+# generate backend p12 certificate from frontend certificate
+openssl pkcs12 -inkey ./frontend/.cert/key.pem -in ./frontend/.cert/cert.pem -export -passout pass:vax-warden -out ./backend/src/main/resources/cert.pfx
+
 if [[ $num_args -eq 1 && "$1" == "--clean" ]]; then
     ./mvnw clean install
 else
